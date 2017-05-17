0657f5d1160cca1eab4882c8f0ff08363bcb5f9c
DaniVSainz <danivsainz@gmail.com>
2017-05-17 02:21:40 -0400

    Fix imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { FavoritesPage } from '../pages/favorites/favorites';
-import { LibraryPage } from '../pages/LibraryPage/LibraryPage';
-import { QuotesPage } from '../pages/QuotesPage/QuotesPage';
-import { QuotePage } from '../pages/QuotePage/QuotePage';
-import { SettingsPage } from '../pages/SettingsPage/SettingsPage';
+import { LibraryPage } from '../pages/library/library';
+import { QuotesPage } from '../pages/quotes/quotes';
+import { QuotePage } from '../pages/quote/quote';
+import { SettingsPage } from '../pages/settings/settings';
 
 
 
